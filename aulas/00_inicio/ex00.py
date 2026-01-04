c2281b5762f6a0078b1f40d49542a2356e030f3d
Diego Morais <dolivmorais@gmail.com>
2026-01-04 12:01:30 -0300

    iniciando estudos com python

---
+++ aulas/00_inicio/ex00.py
@@ -0,0 +1,19 @@
+#%%
+print ("Aprendendo Python")
+#%%
+# variaveis
+idade = 30
+nome = "Diego"
+
+print (nome)
+print (idade)
+#%%
+
+listas = [1, 2, 3, 4, 5] 
+print (listas)
+#%%
+
+#metodos de listas
+listas.append(6)
+print (listas)
+#%%
