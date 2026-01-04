bb97f24c8728eeb28152eeff5efac627e78a4309
Diego Morais <dolivmorais@gmail.com>
2026-01-04 18:45:51 -0300

    adicionando primeiro exercicio

---
+++ a01/ex00.py
@@ -0,0 +1,16 @@
+'''
+print("Olá, Mundo!")
+print(1+2)
+print('a'+'b')
+
+a = input('digite algo:')
+print(a)
+
+a = input('digite algo:')
+print(len(a))
+
+a = int(input('n1:'))
+b = int(input('n2:'))
+print(int(a) + int(b))
+'''
+
