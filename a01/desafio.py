19728af7c9e00f3683f9c01dadaaa7207c81a5fe
Diego Morais <dolivmorais@gmail.com>
2026-01-04 18:59:06 -0300

    adicionando primeiro exercicio de dasafio

---
+++ a01/desafio.py
@@ -0,0 +1,11 @@
+# desafio
+#solicita o nome do usuário
+#solicita o valor do salario
+#valor do bonus
+#calcula o salario final
+#exibe o resultado
+nome = str(input("digite seu nome"))
+salario = float(input("diigite o valor do salario"))
+bonus = float(input("digit o valor do bonus"))
+salario_final = float(salario + bonus)
+print(f"Ola, nome, seu {salario} final é de:, {salario_final}")
